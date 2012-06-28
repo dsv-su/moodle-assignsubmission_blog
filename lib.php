57fafcbff8be80f97bde1cb26ce0cb7b5e281df6
Erik <lundbergerik@gmail.com>
2012-06-28 23:31:16 +0200

    Adding a listener that listens for new blogentries associated with an assignment.

--- lib.php
+++ lib.php
@@ -1,6 +1,48 @@
 <?php
 
 function entry_added_handler($entry) {
+	global $CFG, $USER, $DB;
+	if (isset($entry->modassoc)) {
+		$context_data = $DB->get_record('context', 
+				array('id' => $entry->modassoc));
+		
+		$cm = get_coursemodule_from_id('assign', $context_data->instanceid);
+		if ($cm->modname == 'assign') {
+			$course = $DB->get_record('course', array('id' => $cm->course));
+
+			// Replace this variable with a function call?
+			$context = get_context_instance_by_id($entry->modassoc);
+			$blogsubmission_active = $DB->get_record('assign_plugin_config', 
+					array('assignment' => $cm->instance, 
+					'plugin' => 'blog', 
+					'subtype' => 'assignsubmission',
+					'name' => 'enabled'));
+			
+			//This is a workaround for MDL-27629
+			if ($blogsubmission_active && has_capability('mod/assign:submit', $context)) {
+				require_sesskey();
+				
+				// Since assign::get_user_submission is private, we need to replicate it's
+				// functionallity
+				
+				$existing_submission = $DB->get_record('assign_submission', 
+						array('assignment' => $cm->instance, 'userid' => $USER->id));
+				if ($existing_submission) {
+					$existing_submission->timemodified = time();
+					$DB->update_record('assign_submission', $existing_submission);
+				} else {
+					$new_submission = new stdClass();
+					$new_submission->assignment = $cm->instance;
+					$new_submission->userid = $USER->id;
+					$new_submission->timecreated = time();
+					$new_submission->timemodified = $new_submission->timecreated;
+					$new_submission->status = 'submitted';
+					$DB->insert_record('assign_submission', $new_submission);
+				}	
+				// Here be logging!
+			}
+		}
+	}
 	return true;
 }
 
