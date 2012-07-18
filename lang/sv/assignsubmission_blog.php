1494b78564e69074c413062b51334e6b47cf57ee
Erik Lundberg <lundbergerik@gmail.com>
2012-07-18 16:51:11 +0200

    Adding a swedish translation

---
+++ lang/sv/assignsubmission_blog.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Strings for component 'assignsubmission_blog', language 'sv'
+ *
+ * @package assignsubmission_blog
+ * @copyright 2012 Department of Computer and System Sciences,
+ *					Stockholm University  {@link http://dsv.su.se}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['blog'] = 'Blogginlämning';
+$string['default'] = 'Automatiskt aktiverad';
+$string['default_help'] = 'Om vald kommer den här inlämningsmetoden aktiveras för alla nya uppgifter.';
+$string['numcomment'] = '{$a} kommentar';
+$string['numcomments'] = '{$a} kommentarer';
+$string['enabled'] = 'Blogginlämning';
+$string['enabled_help'] = 'Om vald kan studenter lämna in deras inlämning i form av ett blogginlägg associerat till uppgiften.';
+$string['numentry'] = '{$a} inlägg';
+$string['numentries'] = '{$a} inlägg';
+$string['requiredentries'] = 'Krav på antal blogginlägg';
+$string['requiredentries_help'] = 'Om blogginlämning är aktiverad behöver studenter lämna in detta antal inlägg.';
+$string['requiredcomments'] = 'Krav på antal bloggkommentarer';
+$string['requiredcomments_help'] = 'Om blogginlämning är aktiverad behöver studenter lämna detta antal kommentarer på inlägg.';
+$string['pluginname'] = 'Blogginlämning';
