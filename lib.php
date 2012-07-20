b83b0b7dac62e3aae552d34fa83dbb5278b2007a
Erik Lundberg <lundbergerik@gmail.com>
2012-07-20 09:40:00 +0200

    I just realized that the submission is not removed when the submitted material is removed. This means that much work with the lib.php-file have been in vain.

--- lib.php
+++ lib.php
@@ -64,21 +64,6 @@ function blogsubmission_is_active($assigninstance) {
 }
 
 /**
- * Checks if a user have submitted blog entries that is associated with an assignment, identified by it's contexid.
- *
- * @param int $userid
- * @param int $contextid
- * @return bool
- */
-function user_have_associated_entries($userid, $contextid) {
-    global $DB;
-
-    $usersentries = $DB->count_records_sql('SELECT COUNT(p.id) FROM {post} p, {blog_association} ba '
-            .'WHERE p.id = ba.blogid AND p.userid = ? AND ba.contextid = ?', array($userid, $contextid));
-    return $usersentries > 0;
-}
-
-/**
  * Check if a user have a registered submission to an assignment.
  *
  * @param mixed $userid
@@ -135,75 +120,5 @@ function entry_added_handler($entry) {
         }
     }
 
-    return true;
-}
-
-/**
- * This function removes a submission from the assign_submission table if the user have no associated entries to this assignment.
- *
- * @param int $userid
- * @param int $contextid
- * @param int $instanceid
- * @return void
- */
-function remove_submission_if_no_associated_entries($userid, $contextid, $instanceid, $cm = null) {
-    global $DB;
-
-    if (!user_have_associated_entries($userid, $contextid)
-            && user_have_registred_submission($userid, $instanceid)) {
-        $DB->delete_records('assign_submission', array(
-            'assignment' => $instanceid, 
-            'userid' => $userid));
-        if ($cm == null) {
-            $context = get_context_instance_by_id($contextid);
-            $cm = get_coursemodule_from_id('assign', $context->instanceid);
-        }
-        add_to_log($cm->course, 'assign', 'delete', 'view.php?id='.$cm->id,
-                'Assignment blog submission: Submission removed', $cm->id, $userid);
-    }
-}
-
-/**
- * Handles an edited entry in the blog.
- * If the modassoc value of the entry is 0, then the module association for this entry have been removed.
- * If the module association have been removed, check if there are any assignments where the user have an registred submission
- * but not associated blog entries. If so, remove the registred submission.
- *
- * @param mixed $entry
- * @return bool
- */
-function entry_edited_handler($entry) {
-    global $DB;
-
-    if (isset($entry->modassoc) && $entry->modassoc === '0') {
-        $usersubmissions = $DB->get_records('assign_submission', array(
-            'userid' => $entry->userid
-        ));
-        foreach ($usersubmissions as $index => $submission) {
-            if (blogsubmission_is_active($submission->assignment)) {
-                // Get the contextid for the assignment that the submission is submitted to
-                $contextid = $DB->get_field_sql("SELECT c.id FROM {context} c JOIN {course_modules} cm ON c.instanceid = cm.id"
-                        ." WHERE c.contextlevel = ? AND cm.instance = ?", array(CONTEXT_MODULE, $submission->assignment));
-
-                remove_submission_if_no_associated_entries($entry->userid, $contextid, $submission->assignment);
-            }
-        }
-    }
-    return true;
-}
-
-/**
- * Handles a removed entry in the blog.
- * If the user doesn't have any associated entries left, the assignment submission will be removed.
- *
- * @param mixed $entry
- * @return bool True to indicate that the event was handled successfully.
- */
-function entry_deleted_handler($entry) {
-    global $DB;
-
-    if (($cm = entry_is_relevant($entry))) {
-        remove_submission_if_no_associated_entries($entry->userid, $entry->modassoc, $cm->instance, $cm);
-    }
     return true;
 }
